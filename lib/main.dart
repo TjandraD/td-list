86bf92072898e7b8e524c7643d1a300d130b0c28
TjandraD <t.darmo.t@gmail.com>
2021-01-19 10:58:55 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TD List',
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
